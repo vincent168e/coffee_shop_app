54891e7bc3f00689c488331323c2ce79e60dd094
Vincent Tsang <vincent168e@gmail.com>
2024-02-01 16:10:55 +0800

    Add TapNavigator component

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
+import TabNavigator from './src/navigators/TabNavigator';
 import DetailsScreen from './src/screens/DetailsScreen';
 import PaymentScreen from './src/screens/PaymentScreen';
 
@@ -11,6 +11,10 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen
+          name="Tab"
+          component={TabNavigator}
+          options={{animation: 'slide_from_bottom'}}></Stack.Screen>
         <Stack.Screen
           name="Details"
           component={DetailsScreen}
